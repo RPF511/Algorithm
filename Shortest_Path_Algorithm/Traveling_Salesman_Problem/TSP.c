9cb1a7362451367cd27a21187054e2365cab7992
RPF511 <sbk98726@gmail.com>
2019-10-29 22:59:45 +0900

    TSP with path

--- Shortest_Path_Algorithm/Traveling_Salesman_Problem/TSP.c
+++ Shortest_Path_Algorithm/Traveling_Salesman_Problem/TSP.c
@@ -1,17 +1,6 @@
 //Traveling Salesman Problem
 #include <limits.h>
 #include "multiplearray.h"
-//Print bits
-#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
-#define BYTE_TO_BINARY(byte)  \
-  (byte & 0x80 ? '1' : '0'), \
-  (byte & 0x40 ? '1' : '0'), \
-  (byte & 0x20 ? '1' : '0'), \
-  (byte & 0x10 ? '1' : '0'), \
-  (byte & 0x08 ? '1' : '0'), \
-  (byte & 0x04 ? '1' : '0'), \
-  (byte & 0x02 ? '1' : '0'), \
-  (byte & 0x01 ? '1' : '0') 
 
 //struct circular linked list
 typedef struct circle{
