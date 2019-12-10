a8c4ea344e4d077acd1f0878e0ec619cd5d7c4b4
RPF511 <sbk98726@gmail.com>
2019-12-10 12:33:00 +0900

    bag-bfs

--- bag/bag-bfs/bag_bfs.c
+++ bag/bag-bfs/bag_bfs.c
@@ -34,23 +34,25 @@ void bfs_bag_node(HeapType * heap, int *max,int wmax, int *weight, int *pw,int n
     if(heap -> heapsize > 0){
         delete(heap);
         bagnode * node = heap -> cur;
+        printf("\nnode got\n%d %d %d\nheap ",heap -> cur -> profit, heap -> cur -> weight, heap -> cur -> bound);
+        printh(heap);
         int btemp;
         int ptemp;
-        printf("%d\n",heap -> heapsize);
-        printh(heap);
 
         //printf("%d %d\n",weight[node -> index + 1], node -> weight);
         if(weight[node -> index + 1] + node -> weight <= wmax){
-            printf("%d %d %d",node -> profit,pw[node -> index + 1], weight[node -> index + 1]);
-            ptemp = node -> profit + pw[node -> index + 1] * weight[node -> index + 1];
-            btemp = ptemp + bound(wmax - (weight[node -> index + 1] + node -> weight),weight + (node -> index + 1),pw + (node -> index + 1), noe - node -> index);
-            printf("index %d left profit %d temp %d\n",node -> index + 1,ptemp,btemp);
-            node -> left = mkbagnode(ptemp,weight[node -> index + 1] + node -> weight,btemp,node -> index + 1);
+            //printf("%d %d %d",node -> profit,pw[node -> index + 1], weight[node -> index + 1]);
+            ptemp = node -> profit + pw[node -> index] * weight[node -> index];
+            printf("boundmax %d\n",wmax - (weight[node -> index] + node -> weight));
+            btemp = ptemp + bound(wmax - (weight[node -> index] + node -> weight),weight + (node -> index + 1),pw + (node -> index + 1), noe - node -> index);
+            printf("index %d left profit %d bound %d\n",node -> index + 1,ptemp,btemp);
+            node -> left = mkbagnode(ptemp,weight[node -> index] + node -> weight,btemp,node -> index + 1);
             if(node -> left -> profit > *max){
                 *max = node -> left -> profit;
             }
             if(node -> index + 1 < noe){
                 insert(heap,node -> left -> bound, node -> left);
+                printf("heap ");
                 printh(heap);
             }
         }
@@ -58,13 +60,14 @@ void bfs_bag_node(HeapType * heap, int *max,int wmax, int *weight, int *pw,int n
         ptemp = node -> profit;
         btemp = ptemp + bound(wmax - node -> weight,weight + (node -> index + 1),pw + (node -> index + 1), noe - node -> index);
         node -> right = mkbagnode(ptemp,node -> weight,btemp,node -> index + 1);
-        printf("index %d right profit %d temp %d\n",node -> index + 1,ptemp,btemp);
+        printf("index %d right profit %d bound %d\n",node -> index + 1,ptemp,btemp);
         if(node -> right -> profit > *max){
             *max = node -> right -> profit;
         }
 
         if(node -> index + 1 < noe){
             insert(heap,node -> right -> bound, node -> right);
+            printf("heap ");
             printh(heap);
         }
     }
@@ -73,6 +76,6 @@ void bfs_bag(HeapType * heap,int *max,int wmax,int *weight, int *pw,int noe){
     printf("heapsize %d\n",heap -> heapsize);
     while(heap -> heapsize){
         bfs_bag_node(heap,max,wmax,weight,pw,noe);
-        printh(heap);
+        //printh(heap);
     }
 }
